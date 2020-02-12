a7375dc93a3f7ae49ce6f805ed62f6df3640527d
Rhaisa Alves <rhaisalouiseadm@gmail.com>
2020-02-12 16:24:18 +0100

    Create a color generator with nodejs

--- index.js
+++ index.js
@@ -1,19 +1,24 @@
 var randomColor = require('randomcolor');
-
-const color = randomColor({
-  luminosity: process.argv[3S],
-  hue: process.argv[2],
-});
-
-console.log(color);
 const chalk = require('chalk');
 
-console.log(chalk.hex(color).bold('###############################'));
-console.log(chalk.hex(color).bold('###############################'));
-console.log(chalk.hex(color).bold('###############################'));
-console.log(chalk.hex(color).bold('#########            ##########'));
-console.log(chalk.hex(color).bold(`#########  ${color}   ##########`));
-console.log(chalk.hex(color).bold('#########            ##########'));
-console.log(chalk.hex(color).bold('###############################'));
-console.log(chalk.hex(color).bold('###############################'));
-console.log(chalk.hex(color).bold('###############################'));
+const luminosity = process.argv[3];
+const hue = process.argv[2];
+
+const color = randomColor({
+  luminosity: luminosity,
+  hue: hue,
+});
+
+//console.log(color);
+
+console.log(
+  chalk.hex(color).bold(`###############################
+###############################
+###############################
+#########            ##########
+#########  ${color}   ##########
+#########            ##########
+###############################
+###############################
+###############################`),
+);
